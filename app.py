422e0e683af9dbfeefdb3995777595797814901b
Rohit Wable <rwable10@gmail.com>
2025-01-25 21:35:07 +0530

    pushed method for om-sai-computers

--- app.py
+++ app.py
@@ -21,11 +21,19 @@ firebase_admin.initialize_app(cred)
 # Initialize Firestore
 db = firestore.client()
 
+
 def add_user(data):
     # Add data to Firestore
     doc_ref = db.collection("users").document()  # Auto-generate document ID
     doc_ref.set(data)
     print("User data added successfully!")
+
+
+def add_document(data):
+    # Add data to Firestore
+    doc_ref = db.collection("documents").document()  # Auto-generate document ID
+    doc_ref.set(data)
+    print("Documents data added successfully!")
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
@@ -47,6 +55,23 @@ def submit_form():
 @app.route('/', methods=['GET'])
 def get_data():
     return jsonify({"message": "Welcome to NNR Application!"}), 200
+
+@app.route('/submit_document', methods=['POST'])
+def submit_document():
+    try:
+        # Get form data from the request
+        data = request.json
+        # Get the current time in seconds since the Unix epoch
+        current_time_seconds = time.time()
+
+        # Convert to a readable date and time format
+        readable_time = datetime.fromtimestamp(current_time_seconds).strftime('%d-%m-%Y %H:%M:%S')
+        data["timestamp"] = readable_time
+        add_user(data)
+        return jsonify({"message": "User added successfully!"}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'Failed to add data'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
