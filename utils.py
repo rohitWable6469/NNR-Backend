7c795364e52de4ce82295c92b7087eaac7c5fa1d
Rohit Wable <rwable10@gmail.com>
2025-02-09 00:54:23 +0530

    list all payments

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 def customer_stats_utils(customers, invoices, payments):
-    return {
-        'customers': customers,
-        'invoices': invoices,
-        'payments': payments
-    }
+    for customer in customers:
+        customer_id = customer.get('id')
+        customer['invoices'] = [invoice for invoice in invoices if invoice.get("customerId") == customer_id]
+        customer['payments'] = [payment for payment in payments if payment.get("customerId") == customer_id]
+        customer['invoice_total'] = sum([invoice['total'] for invoice in customer['invoices']])
+        customer['payment_total'] = sum([payment['amount'] for payment in customer['payments']])
+    return customers
+
